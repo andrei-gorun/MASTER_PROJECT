3dfb01ee3ef51cf73444ee0f60057f860b8403c3
Andrei <146974449+andrei-gorun@users.noreply.github.com>
2024-03-12 12:05:03 +0000

    New script for arm
    This version holds the state of the arm, which means it stops it from going limp after a command is executed

---
+++ Dre's_holy_scripture/meng_script.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python 
+
+# To use the API, copy these 4 lines on each Python file you create
+from niryo_robot_python_ros_wrapper.ros_wrapper import *
+import rospy
+import time
+import math
+
+rospy.init_node('niryo_robot_example_python_ros_wrapper')
+
+print "--- Start"
+
+n = NiryoRosWrapper()
+arm_mode = "stiff"          
+try:
+    # Calibrate robot first
+    n.calibrate_auto()
+    print "Calibration finished !"
+
+    time.sleep(1)
+
+    # Move
+    n.set_arm_max_velocity(30)
+    
+    while arm_mode == "stiff":
+    	joint_target = [0.0, 0.0, 0.0, 0.072, 0.032, 1.820]
+    	n.move_joints(*joint_target)
+
+    #n.move_pose(0.245, 0, 0.418, 1.893, -0.032, -0.002)
+        
+
+    #n.shift_pose(ShiftPose.AXIS_X, 0.1)
+    #n.shift_pose(ShiftPose.ROT_PITCH, math.radians(25))
+
+    n.set_learning_mode(True)
+
+
+except NiryoRosWrapperException as e:
+    print e
+    # handle exception here
+    # you can also make a try/except for each command separately
+
+print "--- End"
